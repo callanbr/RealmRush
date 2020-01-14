8e9a500aea6e182373532d4adeb51a11e149e18e
callanbr <chrisallan1992@icloud.com>
2020-01-14 15:48:46 -0700

    added grid snapping code

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour{
+
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+    void Update(){
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize) * gridSize;
+        snapPos.y = 0;
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+    }
+}
