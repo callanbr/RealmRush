cc240d0cdba91b2d5b8171d845e8a0cc0f6753b8
callanbr <chrisallan1992@icloud.com>
2020-01-14 20:25:23 -0700

    cube location and style

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour{
+[SelectionBase]
+public class CubeEditor : MonoBehaviour{
 
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
 
+    TextMesh textMesh;
+
+    void Start(){
+
+    }
+
     void Update(){
         Vector3 snapPos;
+
         snapPos.x = Mathf.RoundToInt(transform.position.x/gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize) * gridSize;
         snapPos.y = 0;
-
         transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
