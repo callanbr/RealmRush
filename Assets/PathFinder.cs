2a4b9b9c8da0d4beefcd18adb1234cf5fb319317
callanbr <chrisallan1992@icloud.com>
2020-01-19 15:12:11 -0700

    change neighbours top color

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -9,9 +9,27 @@ public class PathFinder : MonoBehaviour{
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
 
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
     void Start(){
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions){
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try{
+            grid[explorationCoordinates].SetTopColor(Color.blue);
+            } catch {} //Do nothing for now.
+        }
     }
 
     private void ColorStartAndEnd(){
