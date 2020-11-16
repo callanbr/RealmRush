812135fff7ea34e5ce7a89aeb8cd6e9843649416
callanbr <chrisallan1992@icloud.com>
2020-11-15 20:08:21 -0700

    added enemy hit, spawn, and goal sfx

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -7,6 +7,15 @@ public class EnemyDamage : MonoBehaviour
   [SerializeField] int hitpoints = 10;
   [SerializeField] ParticleSystem hitParticlePrefab;
   [SerializeField] ParticleSystem killParticlePrefab;
+  [SerializeField] AudioClip enemyDamageSFX;
+  [SerializeField] AudioClip enemyDeathSFX;
+
+  AudioSource myAudioSource;
+
+  private void Start()
+  {
+    myAudioSource = GetComponent<AudioSource>();
+  }
 
   private void OnParticleCollision(GameObject other)
   {
@@ -21,10 +30,12 @@ public class EnemyDamage : MonoBehaviour
   void ProcessHit()
   {
     hitpoints--;
+    myAudioSource.PlayOneShot(enemyDamageSFX);
     hitParticlePrefab.Play();
   }
   private void KillEnemy()
   {
+    // myAudioSource.PlayOneShot(enemyDeathSFX);
     var vfx = Instantiate(killParticlePrefab, transform.position, Quaternion.identity);
     vfx.Play();
     float destroyDelay = vfx.main.duration;
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -10,6 +10,7 @@ public class EnemySpawner : MonoBehaviour
   [SerializeField] EnemyMovement enemyPrefab;
   [SerializeField] Transform enemyParentTransform;
   [SerializeField] Text spawnedEnemies;
+  [SerializeField] AudioClip spawnedEnemySFX;
   int score;
 
   // Start is called before the first frame update
@@ -24,6 +25,7 @@ public class EnemySpawner : MonoBehaviour
     while (true) //forever
     {
       AddScore();
+      GetComponent<AudioSource>().PlayOneShot(spawnedEnemySFX);
       var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
       newEnemy.transform.parent = enemyParentTransform;
       yield return new WaitForSeconds(secondsBetweenSpans);
